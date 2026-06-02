5a7779bfd319616a05defe94cfda00fe94423251
Your Name <you@example.com>
2026-06-01 18:50:45 -0600

    fix

--- static/js/chat.js
+++ static/js/chat.js
@@ -64,7 +64,7 @@ class ChatHandler {
         this.historyIndex = -1;
 
         // Available commands for tab autocomplete
-        this.commands = ['help', 'search', 'images', 'geni', 'yt', 'ytdl', 'torrents', 'nyaa', 'budget', 'bills', 'pay', 'addbill', 'firewall', 'news', 'dailynews', 'logs', 'cal', 'contacts', 'mail', 'todo', 'files', '4chan', 'compress', 'convert'];
+        this.commands = ['help', 'search', 'images', 'geni', 'yt', 'ytdl', 'torrents', 'nyaa', 'budget', 'bills', 'pay', 'addbill', 'firewall', 'news', 'dailynews', 'logs', 'mail', 'todo', 'files', '4chan', 'compress', 'convert'];
         
 
         // Load mail accounts for autocomplete
@@ -3801,8 +3801,6 @@ class ChatHandler {
         'bills': ['all', 'paid'],
         'firewall': ['search', 'analyze'],
         'news': ['refresh'],
-        'cal': ['today', 'week', 'add'],
-        'contacts': ['all', 'add', 'search'],
         'mail': ['inbox', 'unread', 'folders', 'folder', 'sum', 'search', 'read', 'summary', 'translate', 'reply', 'forward', 'delete', 'deleteall', 'archive', 'send'],
         // Mail subcommands - will be populated with account names dynamically
         'mail folders': [],
